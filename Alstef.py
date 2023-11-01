be253bf529c68bb5b05c67eb6eb1568a122e9a31
Alex Hope <101165419+MRlolface249@users.noreply.github.com>
2023-11-02 04:12:37 +1300

    Added in Alstef.py file which completes issue #1

---
+++ Alstef.py
@@ -0,0 +1,8 @@
+import os
+
+def main():
+    inputNumber = int(input("Enter a number: ")) #prompts user to enter a number
+    print("The number you entered is: ", inputNumber) #displays the number to be saved to user
+
+if __name__ == "__main__":
+    main()
