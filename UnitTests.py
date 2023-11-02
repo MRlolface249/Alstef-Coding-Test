c90cddbae872394dd8eb8e40a6ef853c61e7f529
MRlolface249 <ahop089@aucklanduni.ac.nz>
2023-11-02 18:21:37 +1300

    implementation of #4 is complete. Tests were created and passed ensuring the acceptance criteria was meet

--- UnitTests.py
+++ UnitTests.py
@@ -5,11 +5,13 @@ from Alstef import main, readPersistedNumber, persistCurrentNumber
 
 class MyTestCase(unittest.TestCase):
 
-    # tests for issue #1
+    # tests for issue #2, tests were modified to fit issue #4
 
     @patch('builtins.input', side_effect=["0"])
     def test_zero(self, mock_input):
-        expected_output = "The number you entered is:  0\n"
+        with open('number.txt', 'w') as file:
+            file.write('0')
+        expected_output = "Your Total Number is:  0\n"
         with patch('sys.stdout', new=io.StringIO()) as fake_out:
             main()
             self.assertEqual(fake_out.getvalue(), expected_output)
@@ -22,40 +24,50 @@ class MyTestCase(unittest.TestCase):
 
     @patch('builtins.input', side_effect=["50"])
     def test_positive(self, mock_input):
-        expected_output = "The number you entered is:  50\n"
+        with open('number.txt', 'w') as file:
+            file.write('0')
+        expected_output = "Your Total Number is:  50\n"
         with patch('sys.stdout', new=io.StringIO()) as fake_out:
             main()
             self.assertEqual(fake_out.getvalue(), expected_output)
 
     @patch('builtins.input', side_effect=["152"])
     def test_on_range(self, mock_input):
-        expected_output = "The number you entered is:  152\n"
+        with open('number.txt', 'w') as file:
+            file.write('0')
+        expected_output = "Your Total Number is:  152\n"
         with patch('sys.stdout', new=io.StringIO()) as fake_out:
             main()
             self.assertEqual(fake_out.getvalue(), expected_output)
 
     @patch('builtins.input', side_effect=["153"])
     def test_over_range(self, mock_input):
-        expected_output = "The number you entered is:  1\n"
+        with open('number.txt', 'w') as file:
+            file.write('0')
+        expected_output = "Your Total Number is:  1\n"
         with patch('sys.stdout', new=io.StringIO()) as fake_out:
             main()
             self.assertEqual(fake_out.getvalue(), expected_output)
 
     @patch('builtins.input', side_effect=["304"])
     def test_on_range_double(self, mock_input):
-        expected_output = "The number you entered is:  152\n"
+        with open('number.txt', 'w') as file:
+            file.write('0')
+        expected_output = "Your Total Number is:  152\n"
         with patch('sys.stdout', new=io.StringIO()) as fake_out:
             main()
             self.assertEqual(fake_out.getvalue(), expected_output)
 
     @patch('builtins.input', side_effect=["305"])
     def test_over_range_double(self, mock_input):
-        expected_output = "The number you entered is:  1\n"
+        with open('number.txt', 'w') as file:
+            file.write('0')
+        expected_output = "Your Total Number is:  1\n"
         with patch('sys.stdout', new=io.StringIO()) as fake_out:
             main()
             self.assertEqual(fake_out.getvalue(), expected_output)
 
-    # tests for issue #2
+    # tests for issue #3
 
     def test_read_previous_number(self):
         fileName = 'testNumber.txt'
@@ -80,6 +92,17 @@ class MyTestCase(unittest.TestCase):
         fileName = 'testNumber.txt'
         if os.path.exists(fileName):
             os.remove(fileName)
+
+    # tests for issue #4
+
+    @patch('builtins.input', return_value='50')
+    def test_main(self, mock_input):
+        with open('number.txt', 'w') as file:
+            file.write('100')
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
+            main()
+            expected_output = "Your Total Number is:  150\n"
+            self.assertEqual(fake_out.getvalue(), expected_output)
 
 if __name__ == '__main__':
     unittest.main()
